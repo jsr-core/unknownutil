26d9523e02281bfb5bb9a6ed569c2a59bb336cda
Alisue <lambdalisue@gmail.com>
2024-08-02 04:52:31 +0900

    :herb: Improve tests of `isPartialOf` and `isRequiredOf`

--- is/partial_of_test.ts
+++ is/partial_of_test.ts
@@ -11,6 +11,7 @@ Deno.test("isPartialOf<T>", async (t) => {
     a: is.Number,
     b: is.UnionOf([is.String, is.Undefined]),
     c: as.Optional(is.Boolean),
+    d: as.Readonly(is.String),
   });
   await t.step("returns properly named function", async (t) => {
     await assertSnapshot(t, isPartialOf(pred).name);
@@ -21,7 +22,10 @@ Deno.test("isPartialOf<T>", async (t) => {
     const a: unknown = { a: 0, b: "a", c: true };
     if (isPartialOf(pred)(a)) {
       assertType<
-        Equal<typeof a, Partial<{ a: number; b: string; c: boolean }>>
+        Equal<
+          typeof a,
+          Partial<{ a: number; b: string; c: boolean; readonly d: string }>
+        >
       >(true);
     }
   });
