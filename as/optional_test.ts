67ef8f9b9e3173606e3a6d7dbd19e3702e41a82b
Alisue <lambdalisue@gmail.com>
2024-08-02 03:05:24 +0900

    :herb: Fix tests for `as/optional.ts`

--- as/optional_test.ts
+++ as/optional_test.ts
@@ -1,58 +1,8 @@
-import { assertEquals } from "@std/assert";
 import { assertSnapshot } from "@std/testing/snapshot";
 import { assertType } from "@std/testing/types";
-import { type Equal, stringify } from "../_testutil.ts";
-import type { Predicate } from "../type.ts";
+import { type Equal, testWithExamples } from "../_testutil.ts";
 import { is } from "../is/mod.ts";
 import { asOptional, asUnoptional } from "./optional.ts";
-
-const examples = {
-  string: ["", "Hello world"],
-  number: [0, 1234567890],
-  bigint: [0n, 1234567890n],
-  boolean: [true, false],
-  array: [[], [0, 1, 2], ["a", "b", "c"], [0, "a", true]],
-  set: [new Set(), new Set([0, 1, 2]), new Set(["a", "b", "c"])],
-  record: [{}, { a: 0, b: 1, c: 2 }, { a: "a", b: "b", c: "c" }],
-  map: [
-    new Map(),
-    new Map([["a", 0], ["b", 1], ["c", 2]]),
-    new Map([["a", "a"], ["b", "b"], ["c", "c"]]),
-  ],
-  syncFunction: [function a() {}, () => {}],
-  asyncFunction: [async function b() {}, async () => {}],
-  null: [null],
-  undefined: [undefined],
-  symbol: [Symbol("a"), Symbol("b"), Symbol("c")],
-  date: [new Date(1690248225000), new Date(0)],
-  promise: [new Promise(() => {})],
-} as const;
-
-async function testWithExamples<T>(
-  t: Deno.TestContext,
-  pred: Predicate<T>,
-  opts?: {
-    validExamples?: (keyof typeof examples)[];
-    excludeExamples?: (keyof typeof examples)[];
-  },
-): Promise<void> {
-  const { validExamples = [], excludeExamples = [] } = opts ?? {};
-  const exampleEntries = (Object.entries(examples) as unknown as [
-    name: keyof typeof examples,
-    example: unknown[],
-  ][]).filter(([k]) => !excludeExamples.includes(k));
-  for (const [name, example] of exampleEntries) {
-    const expect = validExamples.includes(name);
-    for (const v of example) {
-      await t.step(
-        `returns ${expect} on ${stringify(v)}`,
-        () => {
-          assertEquals(pred(v), expect);
-        },
-      );
-    }
-  }
-}
 
 Deno.test("asOptional<T>", async (t) => {
   await t.step("returns properly named function", async (t) => {
@@ -62,8 +12,8 @@ Deno.test("asOptional<T>", async (t) => {
   });
   await t.step("returns proper type predicate", () => {
     const a: unknown = undefined;
-    if (asOptional(is.Number)(a)) {
-      assertType<Equal<typeof a, number | undefined>>(true);
+    if (is.ObjectOf({ a: asOptional(is.Number) })(a)) {
+      assertType<Equal<typeof a, { a?: number }>>(true);
     }
   });
   await t.step("with is.String", async (t) => {
@@ -146,8 +96,8 @@ Deno.test("asUnoptional<T>", async (t) => {
   });
   await t.step("returns proper type predicate", () => {
     const a: unknown = undefined;
-    if (asUnoptional(asOptional(is.Number))(a)) {
-      assertType<Equal<typeof a, number>>(true);
+    if (is.ObjectOf({ a: asUnoptional(asOptional(is.Number)) })(a)) {
+      assertType<Equal<typeof a, { a: number }>>(true);
     }
     if (asUnoptional(is.Number)(a)) {
       assertType<Equal<typeof a, number>>(true);
