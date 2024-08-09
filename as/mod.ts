dce783bb90c46a1014d59b1f32e6e05d3fc6eef7
Alisue <lambdalisue@gmail.com>
2024-08-09 11:24:55 +0900

    chore: asdf

--- as/mod.ts
+++ as/mod.ts
@@ -1,18 +1,16 @@
 import { asOptional, asUnoptional } from "./optional.ts";
 import { asReadonly, asUnreadonly } from "./readonly.ts";
 
-export type AsCollection = {
+/**
+ * Annotation collection for object predicate properties.
+ */
+export const as: {
   /** Annotation test1 */
   Optional: typeof asOptional;
   Readonly: typeof asReadonly;
   Unoptional: typeof asUnoptional;
   Unreadonly: typeof asUnreadonly;
-};
-
-/**
- * Annotation collection for object predicate properties.
- */
-export const as: AsCollection = {
+} = {
   Optional: asOptional,
   /** Annotation test2 */
   Readonly: asReadonly,
