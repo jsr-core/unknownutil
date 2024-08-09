8008c4791cf6511af93462f144169f5d4ef14e60
Alisue <lambdalisue@gmail.com>
2024-08-09 11:20:48 +0900

    docs: annotation test

--- as/mod.ts
+++ as/mod.ts
@@ -1,11 +1,20 @@
 import { asOptional, asUnoptional } from "./optional.ts";
 import { asReadonly, asUnreadonly } from "./readonly.ts";
 
+export type AsCollection = {
+  /** Annotation test1 */
+  Optional: typeof asOptional;
+  Readonly: typeof asReadonly;
+  Unoptional: typeof asUnoptional;
+  Unreadonly: typeof asUnreadonly;
+};
+
 /**
  * Annotation collection for object predicate properties.
  */
-export const as = {
+export const as: AsCollection = {
   Optional: asOptional,
+  /** Annotation test2 */
   Readonly: asReadonly,
   Unoptional: asUnoptional,
   Unreadonly: asUnreadonly,
