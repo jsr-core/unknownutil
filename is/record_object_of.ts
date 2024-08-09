8c1d66cff32c41a4135a75b82891d39534a2e0e7
Milly <milly.ca@gmail.com>
2024-08-09 15:54:54 +0900

    fix[isRecordObjectOf]: checks only own properties

--- is/record_object_of.ts
+++ is/record_object_of.ts
@@ -39,7 +39,8 @@ export function isRecordObjectOf<T, K extends PropertyKey = PropertyKey>(
   return rewriteName(
     (x: unknown): x is Record<K, T> => {
       if (!isRecordObject(x)) return false;
-      for (const k in x) {
+      const keys = Object.keys(x);
+      for (const k of keys) {
         if (!pred(x[k])) return false;
         if (predKey && !predKey(k)) return false;
       }
